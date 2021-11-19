f5be07340eb9d9397800b5310f7c6d00a62f0dfa
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-18 18:43:46 -0800

    sw response

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -25,19 +25,29 @@ self.addEventListener("install", function (evt) {
     self.skipWaiting();
 });
 
-self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Cache Cleared", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
 
-    self.clients.claim();
-});
+
+self.addEventListener('fetch', function(e){
+    if (e.request.url.includes('/api/')){
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache =>{
+                return fetch(e.request)
+                .then(response => {
+                    if (response.status === 200){
+                        cache.put(e.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err =>{
+                    return cache.match(e.request);
+                });
+            }).catch(err => console.log('status error'))
+        )
+        return
+    }
+    e.respondWith(
+        caches.match(e.request).then(function(response){
+            return response || fetch(e.request);
+        })
+    )
+})
